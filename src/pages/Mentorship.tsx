1d4d5373502503b51db3c9a7601d91f6ea4c4cd0
georgemastersclark-7855 <georgemastersclark@googlemail.com>
2026-04-11 22:39:04 +0100

    Update opening copy

--- src/pages/Mentorship.tsx
+++ src/pages/Mentorship.tsx
@@ -29,7 +29,7 @@ const Mentorship = () => {
         {/* Opening letter with left border */}
         <div className="border-l-2 border-white/20 pl-6 mb-12">
           <p className="text-lg font-bold text-white mb-6" style={{ letterSpacing: "-0.02em" }}>
-            I'm taking on 10 serious producers for direct mentorship. When the 10 spots fill, I'm closing the doors and focusing on this group. The next cohort won't be for a while.
+            I'm taking on 10 serious producers for direct mentorship. When the spots fill, I'm closing the doors to focus on getting results for this group. The next cohort won't be for a while.
           </p>
 
           <p className="text-[15px] leading-relaxed text-white/80 mb-4">
